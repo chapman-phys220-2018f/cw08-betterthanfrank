4b311ee7d504f49ef62b66677f4c7f5812919d4b
Amelia Roseto <roseto@chapman.edu>
2018-10-30 17:23:18 +0000

    fixed files

--- test_sinesum.py
+++ test_sinesum.py
@@ -1,12 +1,21 @@
-#!/usr/bin/env python3
+    #!/usr/bin/env python3
 
 """sinesum.py Test Module
-
 Verify implementation of the Fourier sine series using numpy arrays.
 """
-
+import numpy as np
 import sinesum
+import math
 
-def test_dummy():
-    """Dummy test that always fails. Please replace with actual tests."""
-    assert False, "This is a dummy test"
+def sn1():
+ 
+    assert math.isclose(sinesum.sn(np.pi/2, 100000), 1.0, rel_tol=1e-5)
+
+def ft1():
+    assert sinesum.ft(np.pi/2) == 1
+
+def ft2():
+    assert sinesum.ft(0) == 0
+
+def ft3():
+    assert sinesum.ft(-np.pi/2) == -1
